d9d24183beb5015eb41e85bc3b5e6ec3ea0821b4
dinesh <dinkhad123@gmail.com>
2026-05-22 01:41:13 +1000

    test put api

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI(title="Online Ordering System API")
+
+class Item(BaseModel):
+    name: str
+    price: float
+    description: str | None = None
+    is_offer: bool | None = None
 
 @app.get("/")
 def read_root():
@@ -14,6 +21,16 @@ def health() -> dict[str, str]:
 def say_hello(name:str) -> dict [str, str]:
     return {"message": f"Hello, {name}!"}
 
-@app.get("/test")
-def test():
-    return {"message": "Test endpoint"}
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+
+
+
+   
